1990f8a37ba39b44e7a2d271fa5b721874a64cc5
fredericRos <frederic.ros@gmail.com>
2023-06-12 12:18:36 +0200

    Modif 12h17

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("lundi 12 juin 2023 12h14");
+		System.out.println("lundi 12 juin 2023 12h17");
 	}
 
 }
